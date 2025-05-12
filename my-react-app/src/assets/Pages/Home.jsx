238de9f337cf0ada54e26cd5ccb6084d1f0ecd1f
RioZeitsef <metz.matthieu@gmail.com>
2025-05-12 21:58:25 +0200

    adjust the css fort the Header, add the icon in the tab and  install the mui extansion

--- my-react-app/src/assets/Pages/Home.jsx
+++ my-react-app/src/assets/Pages/Home.jsx
@@ -5,7 +5,7 @@ const Home = () => {
     return (
         <div className={Styles.homeContainer}>
             <div className="home">
-                <h1>Welcome to the Home Page</h1>
+                <h1 className="littletitle">Welcome to the Home Page</h1>
                 <p>This is the home page of our React application.</p>
             </div>
         </div>
